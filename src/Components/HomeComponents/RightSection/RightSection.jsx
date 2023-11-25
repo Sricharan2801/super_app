8a81bc78b875701e34ae0cb6900c447336d95cf1
Sri charan <charanbunny3925@gmail.com>
2023-11-26 00:10:17 +0530

    updated news card

--- src/Components/HomeComponents/RightSection/RightSection.jsx
+++ src/Components/HomeComponents/RightSection/RightSection.jsx
@@ -1,38 +1,50 @@
-import React from 'react'
-import styles from "./RightSection.module.scss"
-
+import React, { useState, useEffect } from 'react';
+import styles from "./RightSection.module.scss";
+import axios from 'axios';
 
 const RightSection = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    await axios.get('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=37f24c25d26c4e619ee8a0c89818bb91')
+    .then(res => setData(res.data.articles))
+    .catch(error=> console.log("error"))
+  };
+  
+  useEffect(() => {
+    fetchData();
+   
+  }, []);
+
+
+  const randomNumber = Math.floor(Math.random() * data.length ) 
+ 
+  const articleDescription = data.length > 0 ? data[randomNumber].description : '';
+  const articleImage = data.length >0 ? data[randomNumber].urlToImage : '';
+  const articleTitle = data.length>0 ? data[randomNumber].title : '';
+  const publishedDate = data.length>0 ? data[randomNumber].publishedAt : "";
+
   return (
     <div id={styles.main}>
-
       <div className={styles.news}>
-
         <div className={styles.image}>
-          <img src="./src/Components/HomeComponents/RightSection/mountEverest.png" alt="" />
+          <img src={articleImage} alt="" />
         </div>
 
         <div className={styles.transparentBlock}>
-
-          <p id={styles.question}>Want to climb Mount Everest?</p>
-          <p id={styles.dateAndTime}>2-20-2023|07:35 PM</p>
-
+          <p id={styles.question}>{articleTitle}</p>
+          <p id={styles.dateAndTime}>{publishedDate}</p>
         </div>
 
-
         <div className={styles.contentBox}>
-          <p className={styles.content}>In the years since human beings first reached the summit of Mount Everest
-            in 1953, climbing the world’s highest mountain has changed dramatically.
-            Today, hundreds of mountaineers manage the feat each year thanks to
-            improvements in knowledge, technology, and the significant infrastructure
-            provided by commercially guided expeditions that provide a veritable
-            highway up the mountain for those willing to accept both the......
+          <p className={styles.content}>
+            {/* Displaying author of the first article */}
+            {articleDescription}
           </p>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default RightSection
+export default RightSection;
