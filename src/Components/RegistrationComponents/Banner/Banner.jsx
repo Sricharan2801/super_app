77df25b0b8bb3f0e5977f94f0a7d98944faf2e26
Sri charan <charanbunny3925@gmail.com>
2023-12-03 23:41:42 +0530

    updated registrationPage

--- src/Components/RegistrationComponents/Banner/Banner.jsx
+++ src/Components/RegistrationComponents/Banner/Banner.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import styles from "./Banner.module.css"
 
-
-
-
 const Banner = () => {
   return (
     <div>
-      <img id={styles.image} src=".src/Assets/Image.png" alt=""  />
+      <img id={styles.image} src="../src/Components/RegistrationComponents/Banner/Image.png" alt=""  />
       <p id={styles.caption}>Discover new things on Superapp</p>
     </div>
   )
