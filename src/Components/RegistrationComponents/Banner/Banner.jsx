8a3fa57abc8ab27bede14005b36a3bc4a50ffda8
Sri charan <charanbunny3925@gmail.com>
2023-12-06 22:28:05 +0530

    updated

--- src/Components/RegistrationComponents/Banner/Banner.jsx
+++ src/Components/RegistrationComponents/Banner/Banner.jsx
@@ -4,7 +4,8 @@ import styles from "./Banner.module.css"
 const Banner = () => {
   return (
     <div>
-      <img id={styles.image} src="../src/Components/RegistrationComponents/Banner/Image.png" alt=""  />
+      <img id={styles.image} src="src/Components/RegistrationComponents/Banner/Image.png"
+ alt=""  />
       <p id={styles.caption}>Discover new things on Superapp</p>
     </div>
   )
