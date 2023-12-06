4c9a024d4c123b93369ebbfd2f84a5f7a1d98c3c
Sri charan <charanbunny3925@gmail.com>
2023-12-06 23:03:41 +0530

    updated

--- src/Components/RegistrationComponents/Banner/Banner.jsx
+++ src/Components/RegistrationComponents/Banner/Banner.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import styles from "./Banner.module.css"
+// Banner.jsx
+import React from 'react';
+import styles from './Banner.module.css';
+import Image from '../../../Assets/Images/Image.png';
 
 const Banner = () => {
   return (
     <div>
-      <img id={styles.image} src="src/Components/RegistrationComponents/Banner/Image.png"
- alt=""  />
+      <img id={styles.image} src={Image} alt="" />
       <p id={styles.caption}>Discover new things on Superapp</p>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
